f58e220fbe4e193978b1c93221fdadfd1f14713b
Raman Singh <ramans@evidentpoint.com>
2021-03-11 09:50:26 -0800

    SIMPLY-3578: Fixed crashes when generating cover from book with non ASCII characters

---
+++ TenPrintCover/C64Converter.h
@@ -0,0 +1,14 @@
+//
+//  C64Converter.h
+//  TenPrintCover
+//
+//  Created by Raman Singh on 2021-03-11.
+//  Copyright © 2021 NYPL_Labs. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface C64Converter: NSObject
+
++ (nonnull NSString *)convert: (nullable NSString *)value;
+
+@end
---
+++ TenPrintCover/C64Converter.m
@@ -0,0 +1,54 @@
+//
+//  C64Converter.m
+//  TenPrintCover
+//
+//  Created by Raman Singh on 2021-03-11.
+//  Copyright © 2021 NYPL_Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "C64Converter.h"
+
+@implementation C64Converter
+
++ (NSString *)convert:(NSString *)value {
+    
+    NSString *c64Letters = @" qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlL:zZxXcCvVbBnNmM1234567890.";
+    
+    // returns a string with all the c64-letter available in the title or a random set if none
+    int i, len = (int)value.length;
+    NSMutableString *result = [NSMutableString stringWithCapacity:len];
+    char letter;
+    for (i=0; i<len; i++) {
+        letter = [value characterAtIndex:i];
+        NSRange range = [self indexOf:letter inString:c64Letters];
+        //        NSLog(@"letter: %c num: %d range: %d", letter, (int)letter, range.length);
+        if (range.length == 0) {
+            int anIndex = (int)(letter%c64Letters.length);
+            letter = [c64Letters characterAtIndex:anIndex];
+        }
+        [result appendString:[NSString stringWithFormat:@"%c", letter]];
+    }
+    return [NSString stringWithString:result];
+}
+
++ (NSRange) indexOf:(char) searchChar inString:(NSString *)string {
+    NSRange searchRange;
+    searchRange.location=(unsigned int)searchChar;
+    searchRange.length=1;
+    NSCharacterSet *set = [NSCharacterSet characterSetWithRange:searchRange];
+    
+    // NSCharacterSet:characterSetWithRange by default is never supposed to
+    // return nil. However, on iOS 14.5 beta, it is returning nil when the char
+    // is non ASCII (e.g. accented character), causing the app to crash.
+    if (set) {
+        return [string rangeOfCharacterFromSet:set];
+    }
+    
+    // This is only triggered when passed a non ASCII character e.g. ñ, ó
+    searchRange.location = NSNotFound;
+    searchRange.length = 0;
+    return searchRange;
+}
+
+@end
